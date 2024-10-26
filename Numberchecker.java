60d95ab2605f314118a0609275e27880d8847d4f
Niha159 <niha.23cs081@sode-edu.in>
2024-10-26 10:47:14 +0530

    Add files via upload

---
+++ Numberchecker.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+
+public class Numberchecker {
+    public static void main(String[] args){
+        Scanner sc1=new Scanner(System.in);
+        int n=sc1.nextInt();
+        if(n>0){
+            System.out.println("positive");
+        }
+        else if(n<0)
+        {
+            System.out.println("negative");
+        }
+        
+        
+    }
+    
+}
