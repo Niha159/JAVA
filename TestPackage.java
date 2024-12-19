bd4288d7ddda925a40f40b482f011b0bac633cb0
Niha159 <niha.23cs081@sode-edu.in>
2024-12-19 11:42:58 +0530

    Add files via upload

---
+++ TestPackage.java
@@ -0,0 +1,9 @@
+
+package newpackage;
+import mypack.MyClass;
+public class TestPackage {
+   public static void main(String[] args){
+       MyClass obj1=new MyClass();
+       obj1.display();
+   } 
+}
